656da705bcb100f2e9c54f467af726c30d563a7f
lamkhai0604 <38321198+lamkhai0604@users.noreply.github.com>
2020-04-11 08:03:44 +0700

    final

--- main.js
+++ main.js
@@ -15,14 +15,14 @@ const inventors = [
   
   const numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
   
-  
+  //FILTER
   //1.Print out an array of the inventors whose name starts with 'A'.
   function firstCharA(name) {
     // return name.charAt(0)== "A"
     return name[0] == "A"
   }
   
-  let filterArray = inventors.filter(firstCharA);
+  let filterArray = inventors.filter(firstCharA);  //method filter
   console.log(filterArray)
   
   
@@ -47,7 +47,6 @@ const inventors = [
     }
     return false  
   }
-  
   const result = inventors.filter(test)
   console.log(result);
 
@@ -66,7 +65,7 @@ const inventors = [
   console.log ("Odd number :", newArray)
 
 
-  //Print out an array of the numbers that have two digits.
+  //5.Print out an array of the numbers that have two digits.
   const numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
 
 function twoDigit(number){
@@ -81,8 +80,8 @@ function twoDigit(number){
 let number = numbers.filter(twoDigit)
 console.log("hello :",number)
 
-
-//Print out the first name of each inventor.
+//MAP
+//1.Print out the first name of each inventor.
 function firstName(name){
   for (i = 0; i<name.length; i++) {
     if(name[i]==" "){
@@ -94,7 +93,7 @@ function firstName(name){
  console.log(getFirstName)
 
 
- //Print out the length of every inventor's full name.
+ //2.Print out the length of every inventor's full name.
  function lengthName (name){
   return name.length;
 }
@@ -103,21 +102,32 @@ let filterLengthArray = inventors.map(lengthName);
 console.log(filterLengthArray);
 
 
-//Print out all the inventors' names in uppercase.
+//3.Print out all the inventors' names in uppercase.
 function upperCase (name) {
   return name.toUpperCase();
 }
 let upperCaseName = inventors.map(upperCase);
 console.log(upperCaseName)
 
-//Print out initials of all inventors(e.g. A.E., I.N., ...)
+//4.Print out initials of all inventors(e.g. A.E., I.N., ...)
 function firstInit(name) {
   return name.charAt(0) + name[name.indexOf(' ') + 1]
 }
 filterThirdArray = inventors.map(firstInit);
 console.log(filterThirdArray);
 
-//Print out an array of every number multiplied by 100.
+// inventors.map((name)=>{
+//   let nameSplit = name.split('');
+//   let initials = ''
+//   for(let i = o; i < nameSplit.length; i++){
+//     initials += nameSplit[i].charAt(0);
+//   }
+//   return initials;
+// })
+
+
+
+//5.Print out an array of every number multiplied by 100.
 const numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
 
 function multiplied(num) {
@@ -126,10 +136,112 @@ function multiplied(num) {
 let getMultiplied = numbers.map (multiplied)
 console.log(getMultiplied)
 
-//Print out an array of the powers of two as shown by this array (e.g. 2, 4, 8, 16 ....).
+//6.Print out an array of the powers of two as shown by this array (e.g. 2, 4, 8, 16 ....).
 let newArray = [];
 
 for(let i =1; i<5 ;i ++){
   newArray.push(Math.pow(2,i))
 }
 console.log(newArray)
+
+
+//SORT
+//1.Sort all the inventors in alphabetical order, A-Z.
+inventors.sort()
+console.log(inventors)
+
+//2.Sort all the inventors in reverse alphabetical order, Z-A. Do not use the reverse method.
+
+
+//4.Sort all the inventors by length of name, longest name first. Do not use the reverse method.
+inventors.sort((a,b)=>{
+  // let lengthA = a.length;
+  // let lengthB = b.length;
+  // console.log("a name",a,"length of a :",lengthA)
+  // console.log("b name",b,"length of b :",lengthB)
+  return a.length < b.length? :1-1
+})
+
+//5
+let fizzBuzz = ( num) => {
+  for ( let i = 1; i <= num ; i ++){
+    if (i % 15 === 0 ) console.log ("fizzBuzz")
+    else if (i % 3 === 0) console.log("fizz")
+    else if ( i % 5 === 0 )console.log("buzz")
+    else console.log(i)
+  }
+}
+fizzBuzz(15)
+
+
+ let str="hello"
+ let reverse = (str) => {
+   let array = str.split('');
+   let reverseArr = array.reverse();
+   let result = reverseArr.join('')
+   console.log(result)
+ }
+ reverse(str)
+ 
+ 
+const array = [1,2,3]
+let mirror = (arr) => {
+  console.log(arr)
+  let newArray = [...arr]
+  newArray.reverse()
+  let result = arr.concat(newArray)
+  console.log(result)
+}
+
+mirror(array)
+
+function isPrime (num) {
+  if(num >= 2){
+  for ( let i = 2; i < num; i++){
+    if(num % i === 0){
+      return false
+    }
+  }
+  return true
+  }
+}
+// isPrime(9)
+function primeNum(number) {
+for ( let i =2; i < number; i++){
+  if(isPrime(i)){
+    console.log(i)
+  }
+}
+}
+primeNum(20)
+
+let string = "I am the good boy"
+
+function reversSentence (sentence){
+  let array = sentence.split(' ')
+  console.log(array)
+  let arrayRevers = []
+  for(let i=0; i<array.length; i++){
+    let singleArray = array[i].split('').reverse().join('')
+    console.log(singleArray)
+    arrayRevers.push(singleArray)
+  }
+  
+  console.log(arrayRevers.join(' '))
+}
+// [a,m] => [m,a] => 'ma'
+reversSentence(string)
+
+let sentence = 'I am am the best best please please hire me as a developer developer please'
+
+function removeDuplicate (sentence){
+  let array = sentence.split(' ')
+  let newArray = []
+  for(let i=0; i<array.length; i++){
+    if(!newArray.includes(array[i])){
+      newArray.push(array[i])
+    }
+  }
+  return newArray.join(' ')
+}
+removeDuplicate(sentence)
